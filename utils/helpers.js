2abb2e7b6459f7ce07443a7d661bb158df4625dc
Alicen Bender <alicene.bender@gmail.com>
2024-04-30 17:01:02 -0400

    format date function now works

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,15 +1,11 @@
 module.exports = {
-    format_date: (date) => {
-      // Format date as MM/DD/YYYY
-      return date.toLocaleDateString();
-    },
-  
-    format_amount: (amount) => {
-      // format large numbers with commas
-      return parseInt(amount).toLocaleString();
-    },
-  
-    is_my_page: (pageUser, userId) => {
-      return pageUser === userId;
-    },
-  };
+  format_date: (date) => {
+    return new Date(date).toLocaleDateString();
+  },
+  format_amount: (amount) => {
+    return parseInt(amount).toLocaleString();
+  },
+  is_my_page: (pageUser, userId) => {
+    return pageUser === userId;
+  },
+};
